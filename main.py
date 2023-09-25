afb7408506306ef0c2653fffdf74903c521e5c01
Ronalds Plostnieks <58863864+ronaldsplostnieks@users.noreply.github.com>
2023-09-25 15:00:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber') # Aplikacijas izskats
 font = ("Arial", 12) # fonts lai informcijas OUTPUTS skaidrāks
 
-layout = [  [sg.Text('Produkta nosaukums', size=(15,1)), sg.InputText(key='-PNAME-', do_not_clear=False)],
+layout = [  [sg.Text('Produkta nosaukums', size=(19,1)), sg.InputText(key='-PNAME-', do_not_clear=False)],
             [sg.Radio("Detaļa", "tips",key='-DETALA-', default=True), sg.Radio("Programmatūra", "tips",key='-PROGRAMMATURA-')],
-            [sg.Text('Skaits', size=(15,1)), sg.InputText(key='-AMOUNT-', do_not_clear=False)],
-            [sg.Text('Cena', size=(15,1)), sg.InputText(key='-PRICE-', do_not_clear=False)],
-            [sg.Text('Numurs', size=(15,1)), sg.InputText(key='-NUMBER-', do_not_clear=False)],
+            [sg.Text('Skaits', size=(19,1)), sg.InputText(key='-AMOUNT-', do_not_clear=False)],
+            [sg.Text('Cena', size=(19,1)), sg.InputText(key='-PRICE-', do_not_clear=False)],
+            [sg.Text('Nosaukums (rediģēšanai)', size=(19,1)), sg.InputText(key='-ENAME-', do_not_clear=False)],
             [sg.Text("Produkti")],
             [sg.Output(size=(5,5), font=font, expand_x=True, expand_y=True,  key = 'outputt')],
             [sg.Button('Pievienot produktu'), sg.Button('Produktu piegāde'), sg.Button('Pārdot'), sg.Button('Rediģēt datus'), sg.Button('Beigt')]]
@@ -19,6 +19,13 @@ products = []
 money = 0
 while True:
     event, values = window.read()
+
+    for i in range(len(products)):
+        check = products[i].getPName(values['-ENAME-'])
+        print(check) 
+        if check == 'yes':
+            num = i
+
     if event == sg.WIN_CLOSED or event == 'Beigt':
         break
     elif event == 'Pievienot produktu':
@@ -26,39 +33,39 @@ while True:
         if values["-PNAME-"].lower() == 'dators':
             text = sg.popup_get_text('Datora ražotājs', title="Manufacturer")
             if values['-DETALA-']:
-                products.append(Computer(int(values['-AMOUNT-']), 'detala', float(values['-PRICE-']), values['-NUMBER-'], text))
+                products.append(Computer(int(values['-AMOUNT-']), 'detala', float(values['-PRICE-']), text))
             else:
-                products.append(Computer(int(values['-AMOUNT-']), 'programmatura', float(values['-PRICE-']), values['-NUMBER-'], text))
+                products.append(Computer(int(values['-AMOUNT-']), 'programmatura', float(values['-PRICE-']), text))
         else:
             if values['-DETALA-']:
-                products.append(Shop(values["-PNAME-"], int(values['-AMOUNT-']), 'detala', float(values['-PRICE-']), values['-NUMBER-']))
+                products.append(Shop(values["-PNAME-"], int(values['-AMOUNT-']), 'detala', float(values['-PRICE-'])))
             else:
-                products.append(Shop(values["-PNAME-"], int(values['-AMOUNT-']), 'programmatura', float(values['-PRICE-']), values['-NUMBER-']))
+                products.append(Shop(values["-PNAME-"], int(values['-AMOUNT-']), 'programmatura', float(values['-PRICE-'])))
     elif event == 'Produktu piegāde':
         window.FindElement('outputt').Update('')
-        products[int(values['-NUMBER-'])].shopDelivery(int(values['-AMOUNT-']))
+        products[int(values[num])].shopDelivery(int(values['-AMOUNT-']))
     elif event == 'Pārdot':
         window.FindElement('outputt').Update('')
-        amount, price = products[int(values['-NUMBER-'])].getAmountPrice()
+        amount, price = products[int(values[num])].getAmountPrice()
         if amount >= int(values['-AMOUNT-']):
             money += float(values['-AMOUNT-']) * float(price)
         else:
             money += float(amount) * float(price)
-        products[int(values['-NUMBER-'])].buyProduct(int(values['-AMOUNT-']))
+        products[int(values[num])].buyProduct(int(values['-AMOUNT-']))
     elif event == 'Rediģēt datus':
         window.FindElement('outputt').Update('')
         if values["-PNAME-"].lower() != '':
-            products[int(values['-NUMBER-'])].changeName(values["-PNAME-"])
+            products[int(values[num])].changeName(values["-PNAME-"])
         if values['-AMOUNT-'] != '':
-            products[int(values['-NUMBER-'])].changeAmount(int(values['-AMOUNT-']))
+            products[int(values[num])].changeAmount(int(values['-AMOUNT-']))
         if values['-DETALA-']:
-            products[int(values['-NUMBER-'])].changeType('detala')
+            products[int(values[num])].changeType('detala')
         else:
-            products[int(values['-NUMBER-'])].changeType('programmatura')
+            products[int(values[num])].changeType('programmatura')
         if values['-PRICE-'] != '':
-            products[int(values['-NUMBER-'])].changePrice(float(values['-PRICE-']))
+            products[int(values[num])].changePrice(float(values['-PRICE-']))
 
     for i in range(len(products)):
-        products[i].printShop() 
+        products[i].printShop(i) 
     print('Money earned:', round(money, 2))
 window.close()
--- shop.py
+++ shop.py
@@ -1,14 +1,13 @@
 moneySpent = 0.00
 class Shop:
-    def __init__(self, pname='prece', skaits = 0, gtips ='detaLa', cena=0.0, cipars=0): #Pamata struktūra
+    def __init__(self, pname='prece', skaits = 0, gtips ='detaLa', cena=0.0): #Pamata struktūra
         self.productname = pname
         self.amount = skaits
         self.tips = gtips
         self.price = cena
-        self.number = cipars
 
-    def printShop(self): # Izprinto informāciju
-        print(self.number,'. ', self.productname,', ',"Skaits: ", self.amount,", ", self.tips, ", Cena: ", self.price ,sep='') 
+    def printShop(self, numb): # Izprinto informāciju
+        print(numb,'. ', self.productname,', ',"Skaits: ", self.amount,", ", self.tips, ", Cena: ", self.price ,sep='') 
 
     def buyProduct(self, daudzums): # Preces pārdošana, tiek noņemta prece no saraksta. Ja cenšas nopirkt vairāk, kā ir tad automatiski uzsetto uz 0 un pārdod tikai cik ir.
         if self.amount >= daudzums:
@@ -33,13 +32,17 @@ class Shop:
     
     def changePrice(self, newPrice): #Nomaina produkta cenu
         self.price = float(newPrice)
+    def getPName(self, namee):
+        if self.productname == namee:
+            return 'yes'
+
 
   
 class Computer(Shop): # Klase, ja prece ir 'dators'
-    def __init__(self, skaits, gtips, cena, cipars, razotajs):
-        super().__init__("Dators", skaits, gtips, cena, cipars)
+    def __init__(self, skaits, gtips, cena, razotajs):
+        super().__init__("Dators", skaits, gtips, cena)
         self.manufacture = razotajs
 
-    def printShop(self):
-        print(self.number,'. ', self.manufacture," " , self.productname,', ',"Skaits: ", self.amount,", ", self.tips, ", Cena: ", self.price,sep='')
+    def printShop(self, numb):
+        print(numb,'. ', self.manufacture," " , self.productname,', ',"Skaits: ", self.amount,", ", self.tips, ", Cena: ", self.price,sep='')
 
